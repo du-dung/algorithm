21b1fb2f970709af7de96212eba97f36517953b8
Hyejin Park <triaina0@gmail.com>
2020-02-19 15:03:45 +0900

    Create BOJ_15665_N과M(11)_200219.cpp

---
+++ BOJ_15665_N과M(11)_200219.cpp
@@ -0,0 +1,45 @@
+/* 15665. N과 M(11) Silver 2
+https://www.acmicpc.net/problem/15665
+*/
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+int N, M;
+int num[8], res[8];
+unordered_set<int> s;
+
+void perm(int r) { //r은 고른 숫자 개수
+	if (M == r) {
+		int k = 0;
+		for (int i = 0; i < M; i++) {
+			k *= 10;
+			k += res[i];
+		}
+		if (s.end() != s.find(k))	return; //해당 원소가 이미 있음 -> 출력 x
+		s.insert(k);
+		for (int i = 0; i < M; i++) {//출력
+			printf("%d ", res[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		res[r] = num[i];
+		perm(r + 1);
+	}
+}
+
+int main() {
+	//input
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	//solve
+	sort(num, num + N);
+	perm(0);
+}
